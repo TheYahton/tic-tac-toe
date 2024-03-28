63d3bd0bbcb12c00143da99c48d0912d06215aef
Yahton <>
2024-03-28 21:50:23 +0300

    grid

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import arcade
 
 SIZE = 400
+GRID_COLOR = 138, 138, 138
+GRID_LINE_WIDTH = 5
 
 
 class Tictactoe(arcade.Window):
     def __init__(self):
         super().__init__(SIZE, SIZE, "Tic Tac Toe")
         arcade.set_background_color((51, 51, 51))
+    
+    def draw_grid(self):
+        arcade.draw_line(SIZE/3, 0, SIZE/3, SIZE, GRID_COLOR, GRID_LINE_WIDTH)
+        arcade.draw_line(SIZE/3*2, 0, SIZE/3*2, SIZE, GRID_COLOR, GRID_LINE_WIDTH)
+        arcade.draw_line(0, SIZE/3, SIZE, SIZE/3, GRID_COLOR, GRID_LINE_WIDTH)
+        arcade.draw_line(0, SIZE/3*2, SIZE, SIZE/3*2, GRID_COLOR, GRID_LINE_WIDTH)
 
     def setup(self):
         pass
 
     def on_draw(self):
         self.clear()
+        self.draw_grid()
 
 
 def main():
