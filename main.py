ccd7a0d623a2fb616b587f0a05100b698ccfe45c
Yahton <>
2024-03-29 04:50:38 +0300

    refactor: rethinked setup usage, no more hardcoded figures

--- main.py
+++ main.py
@@ -3,79 +3,55 @@ import arcade
 SIZE = 400
 TILE_SIZE = SIZE // 3
 GRID_COLOR = 138, 138, 138
-GRID_LINE_WIDTH = 5
-NOUGHT_COLOR = 253, 71, 85
-CROSS_COLOR = 1, 208, 251
-DELTA = 20
+BG_COLOR = (51, 51, 51)
 
 
 class Tictactoe(arcade.Window):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(SIZE, SIZE, "Tic Tac Toe")
-        arcade.set_background_color((51, 51, 51))
+        arcade.set_background_color(BG_COLOR)
+        self.grid_field = None
+        self.move = None
+        self.grid = None
+        self.figure_list = None
+
+    def setup(self) -> None:
         self.grid_field = ["blank"] * 9  # can be "blank", "cross" or "nought"
         self.move = "cross"  # cross moves first
-
-    def draw_grid(self):
-        arcade.draw_line(SIZE / 3, 0, SIZE / 3, SIZE, GRID_COLOR, GRID_LINE_WIDTH)
-        arcade.draw_line(
-            SIZE / 3 * 2, 0, SIZE / 3 * 2, SIZE, GRID_COLOR, GRID_LINE_WIDTH
-        )
-        arcade.draw_line(0, SIZE / 3, SIZE, SIZE / 3, GRID_COLOR, GRID_LINE_WIDTH)
-        arcade.draw_line(
-            0, SIZE / 3 * 2, SIZE, SIZE / 3 * 2, GRID_COLOR, GRID_LINE_WIDTH
-        )
-
-    @staticmethod
-    def draw_nought(x, y):
-        arcade.draw_circle_outline(x, y, SIZE / 8, NOUGHT_COLOR, 5)
-
-    @staticmethod
-    def draw_cross(x, y):
-        arcade.draw_line(
-            x + SIZE / 6 - DELTA,
-            y + SIZE / 6 - DELTA,
-            x - SIZE / 6 + DELTA,
-            y - SIZE / 6 + DELTA,
-            CROSS_COLOR,
-            5,
-        )
-        arcade.draw_line(
-            x - SIZE / 6 + DELTA,
-            y + SIZE / 6 - DELTA,
-            x + SIZE / 6 - DELTA,
-            y - SIZE / 6 + DELTA,
-            CROSS_COLOR,
-            5,
-        )
-
-    def draw_noughts_and_crosses(self):
-        for i, value in enumerate(self.grid_field):
-            x = (i % 3 + 1/2) * SIZE/3
-            y = (2 - i // 3 + 1/2) * SIZE/3
-            match value:
-                case "nought":
-                    self.draw_nought(x, y)
-                case "cross":
-                    self.draw_cross(x, y)
-                case _:
-                    pass
+        self.grid = arcade.Sprite("assets/grid.png")
+        self.grid.set_position(SIZE / 2, SIZE / 2)
+        self.figure_list = arcade.SpriteList()
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
-        aim = x // TILE_SIZE + (2-y // TILE_SIZE) * 3
+        aim = x // TILE_SIZE + (2 - y // TILE_SIZE) * 3
         if self.grid_field[aim] != "blank":
             return
         self.grid_field[aim] = self.move
+        self.create_figure(aim)
         self.move = "cross" if self.move == "nought" else "nought"
 
-    def on_draw(self):
+    def on_draw(self) -> None:
         self.clear()
-        self.draw_grid()
-        self.draw_noughts_and_crosses()
+        self.grid.draw()
+        self.figure_list.draw()
+    
+    def create_figure(self, aim) -> None:
+        x = int((aim % 3 + 1 / 2) * SIZE / 3)
+        y = int((2 - aim // 3 + 1 / 2) * SIZE / 3)
+        match self.move:
+            case "nought":
+                nought = arcade.Sprite("assets/nought.png")
+                nought.set_position(x, y)
+                self.figure_list.append(nought)
+            case "cross":
+                cross = arcade.Sprite("assets/cross.png")
+                cross.set_position(x, y)
+                self.figure_list.append(cross)
 
 
 def main():
     game = Tictactoe()
+    game.setup()
     game.run()
 
 
